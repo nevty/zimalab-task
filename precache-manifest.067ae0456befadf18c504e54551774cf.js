e2610b59d07c93701a66aee4f34859b9b9696c6f
nevty <theacutemace@gmail.com>
2020-07-07 01:22:16 +0700

    deploy

---
+++ precache-manifest.067ae0456befadf18c504e54551774cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb182a039bb8841e7f39",
+    "url": "/zimalab-task/css/about.fba5bb1e.css"
+  },
+  {
+    "revision": "29e05d3009c812da4652",
+    "url": "/zimalab-task/css/app.afdce017.css"
+  },
+  {
+    "revision": "d0fd06275e2d6b98d167",
+    "url": "/zimalab-task/css/chunk-vendors.692461fb.css"
+  },
+  {
+    "revision": "67e85472dc13b2c2db85a078cedf2f3b",
+    "url": "/zimalab-task/index.html"
+  },
+  {
+    "revision": "bb182a039bb8841e7f39",
+    "url": "/zimalab-task/js/about.1fdc2632.js"
+  },
+  {
+    "revision": "29e05d3009c812da4652",
+    "url": "/zimalab-task/js/app.1252bdbf.js"
+  },
+  {
+    "revision": "d0fd06275e2d6b98d167",
+    "url": "/zimalab-task/js/chunk-vendors.501422eb.js"
+  },
+  {
+    "revision": "b9dc75d1f936d45d1ed7eed9a6c477dc",
+    "url": "/zimalab-task/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/zimalab-task/robots.txt"
+  }
+]);
